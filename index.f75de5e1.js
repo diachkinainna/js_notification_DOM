596ed8db90334beb4031a09bd9685d1a599e55f1
diachkinainna <diachkinainna@gmail.com>
2024-12-26 22:50:15 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+document.body.style.position = "relative";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    const notificationTitle = document.createElement("h2");
+    const notificationMessage = document.createElement("p");
+    notification.className = `notification ${type}`;
+    notification.style.display = "block";
+    notificationTitle.textContent = title;
+    notificationTitle.className = "title";
+    notificationTitle.style.whiteSpace = "nowrap";
+    notificationMessage.innerHTML = description.replace(/\n/g, "<br>");
+    notification.appendChild(notificationTitle);
+    notification.appendChild(notificationMessage);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
